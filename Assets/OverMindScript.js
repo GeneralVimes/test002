a4a618b3103b4cbded1cf5aa13679f14d9b6aa5d
GeneralVimes <aviglau@gmail.com>
2015-04-09 22:22:01 +0300

    Revert "project changed tmp"
    This reverts commit 5eefe2d6c5a9fe97cca0d225bacf7e9ee9e858cd.

---
+++ Assets/OverMindScript.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+var testAr = [1,2,3,4];
+function Start () {
+	Debug.Log("I'm the Overmind!");
+	Debug.Log(testAr);
+	logAr ();
+	
+}
+
+function Update () {
+
+}
+
+function logAr () {
+	var str:String = "Array: ";
+	for (var i=0; i<testAr.length; i++){
+		str+=testAr[i];
+		str+=', ';
+	}
+	Debug.Log(str);
+}
